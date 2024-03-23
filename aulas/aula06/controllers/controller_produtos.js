aa2976455a3889632ba5b7fd4d0fa0248565c771
unknown <2214290113@ACADEMICO.IESB.BR>
2024-03-22 22:00:13 -0300

    continuação aula06

--- aulas/aula06/controllers/controller_produtos.js
+++ aulas/aula06/controllers/controller_produtos.js
@@ -1,48 +1,58 @@
 const produtos = [];
 
 const listarTodos = (req, res) => {
-  res.json(produtos);
-};
+    res.json(produtos);
+}
 
 const exibir = (req, res) => {
   const { produtoEncontrado } = req;
-  req.json(produtoEncontrado);
+  res.json(produtoEncontrado);
 }
 
-const buscarPeloId = (req, res) => {
-  const { produtoId } = req.params; // { chave1: valor, chave2: valor }
+
+const buscarPeloId = (req, res, next) => {
+  const { produtoId } = req.params;
   const produtoEncontrado = produtos.find(item => item.id == produtoId);
   if (produtoEncontrado) {
-    req.produtoEncontrado = produtoEncontrado
-    next()
+    req.produtoEncontrado = produtoEncontrado;
+    next();
   } else {
-    res.status(404).json({ msg: "Produto não Encontrado" });
+    res.status(404).json({msg: "Produto não encontrado"});
   }
-};
+}    
+
+const validarDados = (req, res, next) => {
+  const { nome, preco } = req.body;
+  if (nome && preco) {
+    next();
+  } else {
+    res.status(422).json({ msg: "Nome e preço são obrigatórios" });
+  }
+}
 
 const criar = (req, res) => {
   const { nome, preco } = req.body;
-  const produtoNovo = { id: produtos.length + 1, nome, preco }
+  const produtoNovo = { id: produtos.length+1, nome, preco }
   produtos.push(produtoNovo);
   res.status(201).json(produtoNovo);
-};
+}  
 
 const atualizar = (req, res) => {
-  const { produtoId } = req.params;
   const { nome, preco } = req.body;
-  const produtoEncontrado = req;
+  const { produtoEncontrado } = req;
   produtoEncontrado.nome = nome;
-  produtoEncontrado.preco = preco;
+  produtoEncontrado.preco = preco;     
   res.json(produtoEncontrado);
-};
+}
 
 const remover = (req, res) => {
-  const { produtoId } = req.params;
-  const posicao = produtos.findIndex(item => item.id == produtoId);
-  if (posicao >= 0) {
-    produto.splice(posicao, 1);
-    res.status(204).end();
-  }
-};
+   const { produtoId } = req.params;
+   const posicao = produtos.findIndex(item => item.id == produtoId);
+   if (posicao >= 0)  {
+     produtos.splice(posicao, 1);
+     res.status(204).end(); 
+   }
+}
 
-module.exports = { listarTodos, buscarPeloId, criar, atualizar, remover, exibir };
+module.exports = { listarTodos, exibir, buscarPeloId, validarDados, 
+  criar, atualizar, remover }
